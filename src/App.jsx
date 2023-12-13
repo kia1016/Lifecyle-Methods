3bc375a3836ef4e391649581e5ceeab5399269b9
kia1016 <kia1016@gmail.com>
2023-12-12 20:57:04 -0500

    React Lifecyle

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import {Component} from "react"
+import "./App.css";
+import FilmsList from "./components/filmsList";
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: ["ready", "set", "GO"],
+      text: "",
+    };
+
+    this.onSubmit= this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+  let newList = [...this.state.list, this.state.text];
+  this.setState({ list: newList, text:""});
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <form onSubmit={this.onSubmit}>
+        <input 
+        type="text" 
+        name="text" 
+        id="text" 
+        value={this.state.text} 
+        onChange={(event) => this.setState({text: event.target.value})}
+        />
+        <button type="submit">Add</button>
+        </form>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return <li key={item + index}>{item}</li>
+          })}
+        </ul>
+        <FilmsList/>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
